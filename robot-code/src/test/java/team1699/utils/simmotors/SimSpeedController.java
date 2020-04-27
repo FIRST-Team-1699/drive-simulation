18286a11f64e43f5ab85fd7f9e7ac260cd30a549
Jakob Misbach <jakobmisbach8@gmail.com>
2020-04-27 19:26:38 -0400

    Create SimSpeedController.java

---
+++ robot-code/src/test/java/team1699/utils/simmotors/SimSpeedController.java
@@ -0,0 +1,41 @@
+package team1699.utils.simmotors;
+
+import edu.wpi.first.wpilibj.SpeedController;
+
+public class SimSpeedController implements SpeedController {
+
+    @Override
+    public void set(double speed) {
+
+    }
+
+    @Override
+    public double get() {
+        return 0;
+    }
+
+    @Override
+    public void setInverted(boolean isInverted) {
+
+    }
+
+    @Override
+    public boolean getInverted() {
+        return false;
+    }
+
+    @Override
+    public void disable() {
+
+    }
+
+    @Override
+    public void stopMotor() {
+
+    }
+
+    @Override
+    public void pidWrite(double output) {
+
+    }
+}
