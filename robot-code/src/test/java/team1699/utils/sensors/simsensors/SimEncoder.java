0cc492e6523ca87e29fdbf73ff27b2729b494679
NPutorti <64275711+NPutorti@users.noreply.github.com>
2020-04-27 20:32:39 -0400

    Update SimEncoder.java

--- robot-code/src/test/java/team1699/utils/sensors/simsensors/SimEncoder.java
+++ robot-code/src/test/java/team1699/utils/sensors/simsensors/SimEncoder.java
@@ -4,7 +4,36 @@ import edu.wpi.first.wpilibj.Encoder;
 
 public class SimEncoder extends Encoder {
 
+    private int rotations;
+    private int rate;
+
+
     public SimEncoder(int channelA, int channelB, boolean reverseDirection) {
         super(channelA, channelB, reverseDirection);
     }
+
+@Override
+public int get() {
+    return rotations;
+}
+
+@Override
+public void reset() {
+
+}
+
+@Override 
+public double getRate() {
+    return rate;
+
+}
+
+public void update(double time) {
+    time = rotations * rate;
+}
+
+public void setRate() {
+    
+}
+
 }
