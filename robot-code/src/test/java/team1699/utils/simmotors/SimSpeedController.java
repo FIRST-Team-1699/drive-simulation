aba6b8b7e5f5ec2a23af748faba18dfc8a3d1282
brooke-R <64278555+brooke-R@users.noreply.github.com>
2020-04-27 21:01:21 -0400

    Update SimSpeedController.java

--- robot-code/src/test/java/team1699/utils/simmotors/SimSpeedController.java
+++ robot-code/src/test/java/team1699/utils/simmotors/SimSpeedController.java
@@ -3,37 +3,49 @@ package team1699.utils.simmotors;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class SimSpeedController implements SpeedController {
-    int port;
-    double speed;
-    boolean isInverted;
+    private int port;
+    private double speed;
+    private boolean isInverted;
     
+    /**
+     * 
+     * @param Port the port of the motor
+     */
     public SimSpeedController(int Port){
         this.port = Port;
     }
 
 
-    
+    /**
+     * sets speed to the speed variable
+     */
     @Override
     public void set(double speed) {
         this.speed = speed; 
 
     }
-
+    /**
+     * gets the speed of the motor and then returns it as speed
+     */
     @Override
     public double get() {
-        return 0;
+        return speed;
     }
-
+    /**
+     * sets whether it is inverted or not to a variable
+     */
     @Override
     public void setInverted(boolean isInverted) {
         this.isInverted = isInverted;
 
 
     }
-
+    /**
+     * gets whether it is inverted and sets it to the boolean
+     */
     @Override
     public boolean getInverted() {
-        return false;
+        return isInverted;
     }
 
     @Override
