eb54ef6bba939c5b783c4bb78c2752ed337d18f0
brooke-R <64278555+brooke-R@users.noreply.github.com>
2020-05-01 20:44:38 -0400

    Update SimSpeedControllerTest.java

---
+++ robot-code/src/test/java/team1699/utils/simmotors/SimSpeedControllerTest.java
@@ -0,0 +1,27 @@
+package team1699.utils.simmotors;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class SimSpeedControllerTest{
+    private SimSpeedController simController;
+    public void setup() {simController = new SimSpeedController()}
+
+    public void testIsInverted(){
+        simController.setInverted(true);
+        assertTrue(true, getInverted);
+
+
+
+    }
+    public void testSpeed(){
+        simController.set(ControlMode.speed, 25.5);
+        assertEquals(25.5, speed, 0.001);
+
+    }
+
+
+
+}
